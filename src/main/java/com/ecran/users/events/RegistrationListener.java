53ce4a3d33327a28ef4af30130d3a5e841501a32
zoezn <zxejimenez@gmail.com>
2023-09-06 00:45:08 -0300

    ayudaaaa

--- src/main/java/com/ecran/users/events/RegistrationListener.java
+++ src/main/java/com/ecran/users/events/RegistrationListener.java
@@ -2,16 +2,18 @@ package com.ecran.users.events;
 
 import com.ecran.users.entity.UserEntity;
 import com.ecran.users.services.UserService;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
 import javax.mail.Session;
-import javax.mail.internet.MimeMessage;
 
 @Component
 public class RegistrationListener implements ApplicationListener<OnRegistrationCompleteEvent> {
@@ -33,16 +35,32 @@ public class RegistrationListener implements ApplicationListener<OnRegistrationC
         UserEntity user = event.getUser();
         String recipientAddress = user.getEmail();
         String subject = "ECRAN: Verifique su mail";
-        String confirmationUrl = event.getAppUrl() + "/users/confirm?token=" + user.getUserId();
+//        Una barra mas delante de users
+//        String confirmationUrl = event.getAppUrl() + "/users/confirm?token=" + user.getUserId();
+        String confirmationUrl = "http://localhost:8082/users/confirm?token=" + user.getUserId();
 
         SimpleMailMessage email = new SimpleMailMessage();
-//        MimeMessage msg= new MimeMessage();
-        email.setTo(recipientAddress);
-        email.setSubject(subject);
-//        email.setContent()
-//        email.setContentType()
-        email.setText("Muchas gracias por registrarte en Ecran.lat! Por favor, verifica tu mail." + "\r\n" + "http://localhost:8082" + confirmationUrl);
-//        email.setText("<html><body><h1>¡Hola!</h1><p>Este es un correo con formato HTML.</p></body></html>" + "http://localhost:8082" + confirmationUrl);
-        mailSender.send(email);
+        try {
+            MimeMessage mimeMessage = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
+//            String htmlMsg = "<a href='"+ confirmationUrl + "'> Hello World!</a>";
+            String html =
+                    "<body> <div> Muchas gracias por registrarse en nuestra página. Por favor, verifique su email: </p> <a href='" + confirmationUrl + "'>Verificar</a></div> </body>";
+
+            mimeMessage.setContent(html, "text/html");
+            /** Use this or below line **/
+            helper.setText(html, true); // Use this or above line.
+            helper.setTo(recipientAddress);
+            helper.setSubject(subject);
+            helper.setFrom("ecran.lat@gmail.com");
+            mailSender.send(mimeMessage);
+        } catch (MessagingException ex) {
+            System.out.println(ex);
+        }
+
+//        email.setTo(recipientAddress);
+//        email.setSubject(subject);
+//        email.setText("Muchas gracias por registrarte en Ecran.lat! Por favor, verifica tu mail." + "\r\n" + "http://localhost:8082" + confirmationUrl);//        email.setText("html><body><h1>¡Hola!</h1><p>Este es un correo con formato HTML.</p></body></html>" + "http://localhost:8082" + confirmationUrl);
+//        mailSender.send(email);
     }
 }
